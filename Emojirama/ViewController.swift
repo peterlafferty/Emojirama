5541e1ab96af734e0f97558ee5bf3304409b4a9c
Peter Lafferty <peter.lafferty@webresint.com>
2016-02-22 21:38:53 +0000

    Initial Commit

---
+++ Emojirama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emojirama
+//
+//  Created by Peter Lafferty on 29/09/2015.
+//  Copyright © 2015 Peter Lafferty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
