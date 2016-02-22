e6e6548e71ff70ab303b4b93e9c45360f9ef1974
Peter Lafferty <peadar.lafferty@gmail.com>
2016-02-22 21:38:53 +0000

    launching view from URI, not sure if the way to display the view controller is correct but meh

--- Emojirama/AppDelegate.swift
+++ Emojirama/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         //Fabric.with([Crashlytics.self()])
         
         if let splitViewController = window?.rootViewController as? UISplitViewController {
@@ -29,28 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         return true
-    }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     func application(application: UIApplication, openURL url: NSURL, options: [String: AnyObject]) -> Bool {
@@ -66,21 +45,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    
     func displayEmoji(emoji:Emoji) {
         print(emoji)
         if let splitViewController = window?.rootViewController as? UISplitViewController {
            
-//            if let viewController = UIStoryboard(name: "Main", bundle:
-//                nil).instantiateViewControllerWithIdentifier("bleurgh") as? ViewController {
-//                    viewController.schemeString = url.absoluteString
-//                    viewController.url = url
-//                    navController.viewControllers.append(viewController)
-//                    
-//            }
-            //self.window?.rootViewController = navController
-            
-            
+            if let viewController = UIStoryboard(name: "Main", bundle:
+                nil).instantiateViewControllerWithIdentifier("emojiViewController") as? ViewController {
+                    viewController.emoji = emoji
+                    let navController = UINavigationController(rootViewController: viewController)
+                    splitViewController.showDetailViewController(navController, sender: self)
+
+            }
         }
         
     }
